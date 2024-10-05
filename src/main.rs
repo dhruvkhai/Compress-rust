1f31475928582881d4b3c024659821b96afbf1b7
dhruvkhai <dhruvkhaitan@gmail.com>
2024-10-05 20:12:46 +0530

    this commit is with error

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+extern crate flate2;
+
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use std::io::copy;
+use std::env::args;
+use std::fs::File;
+
+
+
+
+fn main(){
+    println!("dhruv");
+}
---
+++ src/tempCodeRunnerFile.rs
@@ -0,0 +1,6 @@
+extern crate flate2;
+
+// use flate2::Compression;
+// use flate2::write::GzEncoder;
+
+
